aaf815b8b9de601b9caf8fd007e4ac0155575f4b
David Berglin <david.berglin@gmail.com>
2025-11-28 16:43:20 +0100

    update

--- RazorPage/Pages/Friends/Overview.cshtml.cs
+++ RazorPage/Pages/Friends/Overview.cshtml.cs
@@ -2,23 +2,27 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Models.Interfaces;
 using Services.Interfaces;
+using Models.DTO;
 
 namespace RazorPage.Pages.Friends;
 
     public class OverviewModel : PageModel
     {
         private readonly IAddressesService _addressesService;
+        private readonly IAdminService _adminService;
         public List<IFriend> Friends { get; set; } = new List<IFriend>();
-        public List<IAddress> Addresses { get; set; } = new List<IAddress>();
         
         public async Task<IActionResult> OnGet()
         {
-            var addresses = await _addressesService.ReadAddressesAsync(true, false, "Denmark",0, 100);
+            var addresses = await _addressesService.ReadAddressesAsync(true, false, "Sweden", 0, 100); 
+            var info = await _adminService.GuestInfoAsync();
             return Page();
         }
-        public OverviewModel(IAddressesService addressesService)
+     
+        public OverviewModel(IAddressesService addressesService, IAdminService adminService)
         {
             _addressesService = addressesService;
+            _adminService = adminService;
         }
      
     }
